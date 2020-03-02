e3911f06d26fd5497c69b7a2005e59f2c00e3670
1 <1@303-19>
2020-03-02 17:47:58 +0000

    1

--- esoft/Form1.Designer.cs
+++ esoft/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.login_field = new System.Windows.Forms.TextBox();
+            this.password_field = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // login_field
+            // 
+            this.login_field.Location = new System.Drawing.Point(162, 65);
+            this.login_field.Name = "login_field";
+            this.login_field.Size = new System.Drawing.Size(205, 20);
+            this.login_field.TabIndex = 2;
+            // 
+            // password_field
+            // 
+            this.password_field.Location = new System.Drawing.Point(162, 110);
+            this.password_field.Name = "password_field";
+            this.password_field.PasswordChar = '*';
+            this.password_field.Size = new System.Drawing.Size(205, 20);
+            this.password_field.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(479, 219);
+            this.Controls.Add(this.password_field);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.login_field);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(495, 258);
+            this.MinimumSize = new System.Drawing.Size(495, 258);
+            this.Name = "Form1";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox login_field;
+        private System.Windows.Forms.TextBox password_field;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- esoft/Form1.cs
+++ esoft/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace esoft
 {
@@ -15,6 +16,48 @@ namespace esoft
         public Form1()
         {
             InitializeComponent();
+        }
+        private static Boolean auth = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String login = login_field.Text;
+            String password = password_field.Text;
+            int manager_id;
+            int executer_id;
+
+            String connectionString = "Data Source = 303-19\\MSSQLSERVER2; Initial Catalog = esoft; INtegrated Security = True";
+
+            using(SqlConnection connection = new SqlConnection(@connectionString))
+            {
+                connection.Open();
+                SqlCommand com = new SqlCommand($"SELECT [execuret_id] FROM [esoft].[dbo].[executers] WHERE executer_login = '{login}' AND executer_password = '{password}'", connection);
+                using (SqlDataReader reader = com.ExecuteReader())
+                {
+                    if(reader.Read())
+                    {
+                        auth = true;
+                        executer_id = (int)reader[0];
+
+                    }
+                }
+                com = new SqlCommand($"SELECT [manager_id] FROM [esoft].[dbo].[managers] WHERE [manager_login] = '{login}' AND [manager_password] = '{password}'", connection);
+                using (SqlDataReader reader = com.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        auth = true;
+                        manager_id = (int)reader[0];
+
+                    }
+                }
+            }
+
+            if (!auth)
+            {
+                MessageBox.Show("Невервный логин или пароль!");
+            }
+
         }
     }
 }
